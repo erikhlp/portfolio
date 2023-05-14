88f18dcac66f03af631de5dbd8e7acd8ad857c5b
erikhlp <domingueserikh@gmail.com>
2023-05-14 20:14:59 -0300

    Add Logo and Header configuration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-  const [active, setActive] = useState('""')
+  const [active, setActive] = useState('""');
+  // const [toggle, setToggle] = useState(false);
 
   return (
     <nav className={`${styles.paddinX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -14,8 +15,19 @@ const Navbar = () => {
           setActive("");
           window.scrollTo(0, 0);
         }}>
-          <img src={logo} alt="Logo" className='w-9 h-9 object-cotain' />
+          <img src={logo} alt="Logo" className='w-9 h-9 object-contain'/>
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Erikh</p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li key={link.id} className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`} onClick={() => setActive(link.title)}>
+            <a href="{`#${link.id}`}">{link.title}</a>
+          </li>
+          ))}
+        </ul>
+        {/* <div className="sm:hidden flex flex-1 justify-end items-center">
+            <img src={menu} alt="Menu" className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={() => setToggle(!toggle)} />
+        </div> */}
       </div>
     </nav>
   )
